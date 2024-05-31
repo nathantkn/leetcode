fca4f6998c90deb4f48e0892cea8ffb0d3823aba
Nathan Trinh <140400882+nathantkn@users.noreply.github.com>
2024-06-01 02:29:24 +0700

    Update 242-valid-anagram.py

--- 242-valid-anagram.py
+++ 242-valid-anagram.py
@@ -1,3 +1,19 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         return sorted(s) == sorted(t)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        myMap = defaultdict(int)
+
+        for x in s:
+            myMap[x] += 1
+
+        for i in t:
+            myMap[i] -= 1
+
+        for val in myMap.values():
+            if val != 0:
+                return False
+
+        return True
