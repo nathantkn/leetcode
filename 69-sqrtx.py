d6406000cc8cf0d96f56a7179db9c51930f11e1a
Nathan Trinh <140400882+nathantkn@users.noreply.github.com>
2024-08-13 00:47:31 +0700

    Create 69-sqrtx.py

---
+++ 69-sqrtx.py
@@ -0,0 +1,12 @@
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        low, high = 0, x + 1
+
+        while low < high:
+            mid = low + (high - low) // 2
+            if mid * mid > x:
+                high = mid
+            else:
+                low = mid + 1
+
+        return low - 1
